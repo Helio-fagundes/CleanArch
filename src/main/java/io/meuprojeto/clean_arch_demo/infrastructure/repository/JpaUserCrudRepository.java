75fe1a730d29e1d4ab1460128835d17e99ccde73
helio <helioemvezdisso@gmail.com>
2025-09-24 13:09:15 -0300

    Revert "primeiro commit"
    This reverts commit 2fd23e7b4ffe5a85c390a8117b1a0020b45b6f39.

--- src/main/java/io/meuprojeto/clean_arch_demo/infrastructure/repository/JpaUserCrudRepository.java
+++
@@ -1,6 +0,0 @@
-package io.meuprojeto.clean_arch_demo.infrastructure.repository;
-
-import io.meuprojeto.clean_arch_demo.infrastructure.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaUserCrudRepository extends JpaRepository<UserEntity, Long> {}
--- src/main/java/io/meuprojeto/clean_arch_demo/usecase/dto/UserResponseDto.java
+++
@@ -1,3 +0,0 @@
-package io.meuprojeto.clean_arch_demo.usecase.dto;
-
-public record UserResponseDto(Long id, String nome, String email){}
