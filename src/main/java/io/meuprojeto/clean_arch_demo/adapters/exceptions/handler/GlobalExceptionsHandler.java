7667e24a72c33aae7d9b1be5d69ac096aef7af6d
helio <helioemvezdisso@gmail.com>
2025-10-12 20:53:16 -0300

    feat: criação de mais uma exception de EmailAlreadyExistsException

--- src/main/java/io/meuprojeto/clean_arch_demo/adapters/exceptions/handler/GlobalExceptionsHandler.java
+++ src/main/java/io/meuprojeto/clean_arch_demo/adapters/exceptions/handler/GlobalExceptionsHandler.java
@@ -1,6 +1,7 @@
 package io.meuprojeto.clean_arch_demo.adapters.exceptions.handler;
 
 import io.meuprojeto.clean_arch_demo.adapters.exceptions.ExceptionResponse;
+import io.meuprojeto.clean_arch_demo.usecase.exceptions.EmailAlreadyExistsException;
 import io.meuprojeto.clean_arch_demo.usecase.exceptions.NotExistUserException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,5 +14,11 @@ public class GlobalExceptionsHandler {
     public ResponseEntity<ExceptionResponse> handleNotExistingUserException(NotExistUserException ex) {
         ExceptionResponse response = new ExceptionResponse("User not found", ex.getMessage());
         return ResponseEntity.status(404).body(response);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ExceptionResponse> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        ExceptionResponse response = new ExceptionResponse("Email already exists", ex.getMessage());
+        return ResponseEntity.status(400).body(response);
     }
 }
