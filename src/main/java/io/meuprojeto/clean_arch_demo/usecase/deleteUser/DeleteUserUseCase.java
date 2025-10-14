a1ea396d2f61b0028cd9802a2f08396f6dd64aec
helio <helioemvezdisso@gmail.com>
2025-10-13 21:46:19 -0300

    fix: coloquei uns filtros e exceptions

--- src/main/java/io/meuprojeto/clean_arch_demo/usecase/deleteUser/DeleteUserUseCase.java
+++ src/main/java/io/meuprojeto/clean_arch_demo/usecase/deleteUser/DeleteUserUseCase.java
@@ -12,6 +12,9 @@ public class DeleteUserUseCase {
     }
 
     public String execute(Long id) {
+        if(id == null){
+            throw new IllegalArgumentException("ID cannot be null.");
+        }
         if (repository.findById(id).isEmpty()) {
             throw new NotExistUserException("User with ID (" + id + ") does not exist.");
         }
--- src/main/java/io/meuprojeto/clean_arch_demo/usecase/searchUser/SearchUserUseCase.java
+++ src/main/java/io/meuprojeto/clean_arch_demo/usecase/searchUser/SearchUserUseCase.java
@@ -2,6 +2,7 @@ package io.meuprojeto.clean_arch_demo.usecase.searchUser;
 
 import io.meuprojeto.clean_arch_demo.domain.repository.UserRepository;
 import io.meuprojeto.clean_arch_demo.usecase.dto.UserResponseDto;
+import io.meuprojeto.clean_arch_demo.usecase.exceptions.NotExistUserException;
 
 import java.util.Optional;
 
@@ -14,6 +15,12 @@ public class SearchUserUseCase {
     }
 
     public Optional<UserResponseDto> execute(Long  id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID cannot be null.");
+        }
+        if (userRepository.findById(id).isEmpty()) {
+            throw new NotExistUserException("User with ID (" + id + ") does not exist.");
+        }
         return userRepository.findById(id)
                 .map(User -> new UserResponseDto(User.getId(), User.getNome(), User.getEmail()));
     }
